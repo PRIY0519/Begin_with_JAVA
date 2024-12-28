812989343a8da26cd9d3639b5fae21b02f962077
Priya Gupta <147814220+PRIY0519@users.noreply.github.com>
2024-12-28 17:59:52 +0530

    Create Main(Tables).java
    "This code is for making a program that calculates the multiplication table of any number."

---
+++ Main(Tables).java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+   public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter First Number:");
+        int num1=sc.nextInt();
+    
+       for (int i=1;i<=10;i++){
+             System.out.print(num1);
+              System.out.print("x");
+             
+             System.out.print(i);
+              System.out.print("=");
+       System.out.println(num1*i);
+
+        }}}
+       
